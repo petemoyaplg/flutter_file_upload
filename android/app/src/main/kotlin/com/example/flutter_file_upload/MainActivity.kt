8d92755401f281e73a36b6b12a05394e0a95219f
STAGIAIRE IT <stagiaire-it@local.rawsur.com>
2022-05-23 17:53:10 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_file_upload/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_file_upload
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: avoid_print
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<PlatformFile>? _file;
+
+  void _openFileExplorer() async {
+    _file = (await FilePicker.platform.pickFiles(
+      type: FileType.any,
+      allowMultiple: false,
+      allowedExtensions: null,
+    ))!
+        .files;
+
+    print('Path : ${_file!.first.path}');
+    print('Name : ${_file!.first.name}');
+    print('Bytes : ${_file!.first.bytes}');
+    print('Weight : ${_file!.first.size}');
+    print('Extention : ${_file!.first.extension}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('File Upload'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _openFileExplorer,
+              child: const Text('Open File Explorer'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Upload File'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
